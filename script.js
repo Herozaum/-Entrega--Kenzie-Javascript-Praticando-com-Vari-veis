94fa9bed98aa52e6da8f1a3a6abfa65ca4d72822
Herozaum <canalherozaum@hotmail.com>
2022-08-09 20:58:05 -0300

    Entrega

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = "Wesley";
+let sobrenome = "Oliveira"; 
+let anoNascimento = 2002; 
+let anoAtual = 2022; 
+let idade = (anoAtual - anoNascimento); 
+let cidade = "Salvador";
+
+console.log(`Eu sou ${nome +" "+ sobrenome} tenho ${idade} anos e moro em ${cidade}`)
